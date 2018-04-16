da2a7ca99045fce83480ff1fc9f4233fe436eb9b
Laura <feest55@hotmail.com>
2018-04-16 18:38:59 +0200

    First model created

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+class Recipe(models.Model):
+    recipe_name = models.CharField(max_length=200)
+    recipe_category = models.CharField(max_length=200)
+    recipe_url = models.URLField(max_length = 200)
+    recipe_rating = models.IntegerFieldvalidators=([MinValueValidator(0), MaxValueValidator(10)])
